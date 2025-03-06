2060994e24fede2809b953da32d8f2b28d6cd029
nhlmg93 <nhelmig93@gmail.com>
2025-03-06 17:02:11 -0600

    cant be null env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { OpenAI } from "openai";
+import { env } from "~/env";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function createOpenAIClient() {
+  return new OpenAI({
+    apiKey: env.OPENAI_API_KEY,
+  });
 }
